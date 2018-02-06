8e1930c27971b3741658d1d3aba8626bf3ebf583
Fran_MSI\Fran <shaoranhyku@gmail.com>
2018-02-06 09:40:48 +0100

    Añadido botón interactivo.

---
+++ Leap Motion/Assets/Scripts/ButtomManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtomManager : MonoBehaviour {
+
+    // Referencia al componente Audio Source
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        // Recuperamos la referencia al componente Audio Source
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Click()
+    {
+            audioSource.Play();
+    }
+}
